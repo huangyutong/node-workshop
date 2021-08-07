005665426274d625960add16f681cce4d50bfe9a
huangyutong <j412988921241@gmail.com>
2021-08-07 09:55:45 +0800

    write answer

--- promise/quiz1.js
+++ promise/quiz1.js
@@ -1,6 +1,9 @@
 
 // 請問下列程式碼印出的順序為何？
-//Ans:
+//Ans:4-1-3-5-2
+//先呼叫console.log(4)再來呼叫syncF function
+//進入function 第一個遇到的是console.log(1)但因setTimeout非同步被丟到Queue
+//再遇到console.log(3)再進行onsole.log(5)等全部stack執行完才執行console.log(2)
 
 function syncF() {
     console.log(1);
--- promise/quiz2.js
+++ promise/quiz2.js
@@ -1,4 +1,7 @@
-//Ans:
+//Ans:4-1-5-2-3
+//依順序console.log(4)->asyncF()
+//進入function,console.log(1),console.log(2)要等一下才繼續進行接下來進行console.log(5
+//console.log(5)執行完才回func繼續執行console.log(2)->console.log(3)
 async function asyncF() {
     console.log(1);
     await new Promise((resolve, reject) => {
