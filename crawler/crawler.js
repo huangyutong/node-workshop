fa3041527e873f84d3698d40606bc2e5fcfb1dbd
huangyutong <j412988921241@gmail.com>
2021-08-07 16:22:55 +0800

    日期除錯

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 const moment = require("moment")
 
-
+// console.log(moment().format = ("YYYYMMDD"))
 axios
     .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?", {
         params: {
             response: "json",
-            date: moment().format = ("YYYYMMDD"),
+            date: moment().format("YYYYMMDD"),
             stockNo: "2330",
         },
     })
