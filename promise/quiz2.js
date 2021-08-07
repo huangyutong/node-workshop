01b2a90839c74eafba866a29e9490035958d070b
huangyutong <j412988921241@gmail.com>
2021-08-07 09:40:56 +0800

    add document

---
+++ promise/quiz2.js
@@ -0,0 +1,15 @@
+//Ans:
+async function asyncF() {
+    console.log(1);
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(2);
+            resolve();
+        }, 0);
+    });
+    console.log(3);
+}
+
+console.log(4);
+asyncF();
+console.log(5);
