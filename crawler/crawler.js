94d4d43f102d8f3ca464ffb1cdd64d5f1a2d8754
huangyutong <j412988921241@gmail.com>
2021-08-07 16:07:55 +0800

    axios & moment.js

---
+++ crawler/crawler.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const moment = require("moment")
+
+
+axios
+    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?", {
+        params: {
+            response: "json",
+            date: moment().format = ("YYYYMMDD"),
+            stockNo: "2330",
+        },
+    })
+    .then((response) => {
+        console.log(response.data);
+    })
