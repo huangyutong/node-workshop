a09a88bfe4706610736afd08aa816f649c16fa56
huangyutong <j412988921241@gmail.com>
2021-08-08 11:04:12 +0800

    stock空行修改

--- crawler/crawler-async-await.js
+++ crawler/crawler-async-await.js
@@ -1,3 +1,42 @@
+// const axios = require("axios");
+// const moment = require("moment")
+// const fs = require("fs")
+
+// // console.log(moment().format = ("YYYYMMDD"))
+// let promiseStock = function () {
+//     return new Promise((resolve, reject) => {
+//         fs.readFile("/crawler/stock.txt", "utf8", (err, stockCode) => {
+//             if (err) {
+//                 reject("error", err)
+//             } else {
+//                 resolve(stockCode)
+//             }
+//         });
+
+//     })
+// }
+// let axiosStock = function (stockCode) {
+//     return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?", {
+//         params: {
+//             response: "json",
+//             date: moment().format("YYYYMMDD"),
+//             stockNo: stockCode,
+//         }
+//     });
+// };
+
+// async function homework() {
+//     try {
+//         let stockCode = await promiseStock();
+//         let response = await axiosStock(stockCode);
+//         console.log(response.data.title);
+//     } catch (error) {
+//         console.log("error", error);
+//     }
+// }
+// homework();
+
+
 const axios = require("axios");
 const moment = require("moment")
 const fs = require("fs")
@@ -16,7 +55,7 @@ let promiseStock = function () {
     })
 }
 let axiosStock = function (stockNo) {
-    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?", {
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
         params: {
             response: "json",
             date: moment().format("YYYYMMDD"),
@@ -25,14 +64,13 @@ let axiosStock = function (stockNo) {
     });
 };
 
-ansyc function homework() {
+async function homework() {
     try {
-        let stockCode = await promiseStock(stockCode);
-        console.log(do1);
-        let response = await axiosStock();
+        let stockCode = await promiseStock();
+        let response = await axiosStock(stockCode.trim());
         console.log(response.data.title);
     } catch (error) {
-        console.log("error");
+        console.log("error", error);
     }
 
 }
