a65f167d7a9b0e63102561cd49b069bfec144640
huangyutong <j412988921241@gmail.com>
2021-08-08 15:34:19 +0800

    新增Process.env.PORT

--- crawler/crawler-db.js
+++ crawler/crawler-db.js
@@ -22,6 +22,7 @@ function promiseStock() {
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
