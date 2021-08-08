1cba8faed5698627a23b0ab3f2950d376d7d6135
huangyutong <j412988921241@gmail.com>
2021-08-08 11:10:07 +0800

    stock空行修改

--- crawler/crawler-cb.js
+++ crawler/crawler-cb.js
@@ -3,27 +3,6 @@ const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 
-// console.log(moment().format = ("YYYYMMDD"))
-
-// fs.readFile("stock.txt", "utf8", (err, stockCode) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         axios
-//             .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-//                 params: {
-//                     response: "json",
-//                     date: moment().format("YYYYMMDD"),
-//                     stockNo: stockCode,
-//                 },
-//             })
-//             .then((response) => {
-//                 console.log(response.data.title);
-//             })
-//     }
-// })
-
-
 fs.readFile("stock.txt", "utf8", (err, stockCode) => {
     if (err) {
         console.error(err);
@@ -33,7 +12,7 @@ fs.readFile("stock.txt", "utf8", (err, stockCode) => {
                 params: {
                     response: "json",
                     date: moment().format("YYYYMMDD"),
-                    stockNo: stockCode, 
+                    stockNo: stockCode.trim()
                 },
             })
             .then((response) => {
