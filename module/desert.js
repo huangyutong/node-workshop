8f724d47488c97d70d91e01012477e190ff678bf
huangyutong <j412988921241@gmail.com>
2021-08-14 14:18:35 +0800

    module作業

---
+++ module/desert.js
@@ -0,0 +1,25 @@
+let a = '巴斯克起司'
+let b = '瑪德蓮'
+let c = ''
+
+function setA(name) {
+    a = name
+}
+function showA() {
+    return a
+}
+function showB() {
+    return b
+}
+function showC() {
+    return c
+}
+
+module.exports = {
+    setA,
+    showA,
+    showB,
+    showB,
+    showC,
+};
+console.log
---
+++ module/index.js
@@ -0,0 +1,7 @@
+let desert = require('./desert')
+
+console.log(desert)
+console.log(desert.showB())
+console.log(desert.showA())
+desert.setA('馬卡龍')
+console.log(desert.showA())
