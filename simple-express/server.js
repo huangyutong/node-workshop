ff50802fc4cf4e7ae79948e9e8af461fbe2ce456
huangyutong <j412988921241@gmail.com>
2021-08-15 12:54:05 +0800

    修改中間件

--- simple-express/server.js
+++ simple-express/server.js
@@ -4,23 +4,29 @@ const express = require('express')
 // 利用 express 建立了一個 express application
 let app = express()
 
+//中間件
 app.use((req, res, next) => {
     let dt = new Date()
-    response.send(`cat ${dt}`)
-    // console.log('cat')
+    console.log(`cat${dt}`)
     next()
 })
 app.use((req, res, next) => {
     let dt = new Date()
-    response.send(`dog${dt}`)
-    // console.log('dog')
+    console.log(`dog${dt}`)
     next()
 })
+//route路由
 // HTTP Method: get, post, put, patch, delete
 app.get('/', function (request, response, next) {
     response.send('Hello~~')
 })
 
-app.listen(3001, function () {
+//stock GET API
+
+app.get('/about', function (request, response, next) {
+    response.send('About')
+})
+
+app.listen(3000, function () {
     console.log('我們的 web server 啟動了～')
 })
